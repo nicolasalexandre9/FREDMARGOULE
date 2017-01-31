0e52b45793974df01c2a1aa9253225a1faa56f34
nicolas alexandre <nicolas alexandre>
2017-01-31 23:28:17 +0100

    Initial commit

---
+++ android/src/com/margoulin/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.margoulin.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.margoulin.game.FredMargGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FredMargGame(), config);
+	}
+}
